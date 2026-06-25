0da28fd7474c07fde41a3c98b6405e12342cd007
N.Janani <jananinagaraj384@gmail.com>
2026-06-25 16:30:34 +0530

    update groq model

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def generate():
 
     try:
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama3-70b-8192",
             messages=[{"role": "user", "content": prompt}]
         )
         text = response.choices[0].message.content
